62ad06fb14bda876f08c44e6456ab3ff319a175a
amog4 <saiamoghc@gmail.com>
2021-09-21 16:02:51 +0530

    MSSQL-Lambda-RDS

---
+++ RDS/lambda_mssql/README.md
@@ -0,0 +1,9 @@
+# Connect mssql rds from Lambda 
+
+* As Lambda is a linux environment package pyodbc requires addition packages like MS odbc driver and unixodbc 
+
+* I followed [this](https://medium.com/@narayan.anurag/breaking-the-ice-between-aws-lambda-pyodbc-6f53d5e2bd26) blog to get the addtional requirements package.zip (very useful)
+
+* Create a layer and push the zip by CLI
+
+
---
+++ RDS/lambda_mssql/access_sql_server.py
@@ -0,0 +1,28 @@
+import pyodbc
+import os
+
+
+
+server = os.getenv('MSSQL_SERVER')
+username  = os.getenv('MSSQL_USERNAME')
+password = os.getenv('MSSQL_PASSWORD')
+
+def sql_server_connect(server ,database,username ,password):
+    conn = pyodbc.connect(r"DRIVER={ODBC Driver 17 for SQL Server};SERVER="+server+';PORT=1433'+';DATABASE='+database+';UID='+username+';PWD='+password)
+    return conn
+
+conn = sql_server_connect(server = server,
+                    database = 'master',
+                    username =username ,
+                    password =password)
+
+
+cur =  conn.cursor()
+
+sql_01 = """
+            SELECT * FROM INFORMATION_SCHEMA.TABLES
+
+           """
+x = cur.execute(sql_01) 
+
+print(x.fetchall())
